59d4f86ca0c1559fa3efe05ae46c039484078bb0
Palatip Jantawong <palatipjant@outlook.com>
2024-02-18 23:37:26 +0700

    240218

---
+++ SwiftUIAuth/Core/Authentication/LoginView.swift
@@ -0,0 +1,33 @@
+//
+//  LoginView.swift
+//  SwiftUIAuth
+//
+//  Created by Palatip Jantawong on 18/2/2567 BE.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                //Image
+                Image("firebase")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical)
+                
+                //Form
+                
+                //Sign in button
+                
+                //Sign up button
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
