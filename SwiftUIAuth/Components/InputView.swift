d876cfd5ef924a1dfb2d1e5ec81cd50b905df602
Palatip Jantawong <palatipjant@outlook.com>
2024-02-19 00:13:42 +0700

    21:19

---
+++ SwiftUIAuth/Components/InputView.swift
@@ -0,0 +1,39 @@
+//
+//  InputView.swift
+//  SwiftUIAuth
+//
+//  Created by Palatip Jantawong on 18/2/2567 BE.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(.gray)
+                .fontWeight(.bold)
+                .font(.footnote)
+            
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            } else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    InputView(text: .constant(""), title: "Email Address", placeholder: "Name@Example.com")
+}
--- SwiftUIAuth/Core/Authentication/LoginView.swift
+++ SwiftUIAuth/Core/Authentication/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         NavigationStack{
             VStack{
@@ -19,10 +23,44 @@ struct LoginView: View {
                     .padding(.vertical)
                 
                 //Form
-                
+                VStack(spacing:24) {
+                    InputView(text: $email, 
+                              title: "Email Address",
+                              placeholder: "example@example.com")
+                    .textInputAutocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter youe password",
+                              isSecureField: true)
+                    .textInputAutocapitalization(.none)
+                    
+                }.padding(.horizontal)
                 //Sign in button
+                Button(action: {
+                    print("user login")
+                }, label: {
+                    Label("Sign in", systemImage: "arrow.right")
+                        .foregroundStyle(.white)
+                        .frame(width: UIScreen.main.bounds.width-32, height: 48)
+                        .background(.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }).padding(.top, 24)
+                
+                Spacer()
                 
                 //Sign up button
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account")
+                        Text("Sign up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+
             }
         }
     }
