316e563dca9fb0ad33d917fc69171bce08429fde
Palatip Jantawong <palatipjant@outlook.com>
2024-02-18 23:05:04 +0700

    Initial Commit

---
+++ SwiftUIAuth/SwiftUIAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthApp.swift
+//  SwiftUIAuth
+//
+//  Created by Palatip Jantawong on 18/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
